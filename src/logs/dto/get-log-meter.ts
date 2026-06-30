4f13f9e1ef72025fdb5782a4859427ea69e96b66
carlosnoguez-traza <carlos.noguez@traza.digital>
2026-06-30 08:50:59 -0600

    doc: documentation of logs routes

--- src/logs/dto/get-log-meter.ts
+++ src/logs/dto/get-log-meter.ts
@@ -1,6 +1,8 @@
 import { IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class GetLogByMeterDto {
+    @ApiProperty({ description: 'UUID del medidor a consultar', example: 'f47ac10b-58cc-4372-a567-0e02b2c3d479' })
     @IsUUID('4', { message: 'El id debe ser un UUID v4 válido' })
     id!: string;
 }
